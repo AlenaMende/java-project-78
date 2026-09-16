575ec73ed649c538295c024817184719a4aa8eca
AlenaMende <alena.zatokoveenko.9696@gmail.com>
2026-09-16 21:30:04 +0300

    update StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -5,17 +5,16 @@ public class StringSchema extends BaseSchema<String> {
     @Override
     public StringSchema required() {
         this.required = true;
-        addCheck("notEmpty", value -> !value.isEmpty());
         return this;
     }
 
     public StringSchema minLength(int length) {
-        addCheck("minLength", value -> value.isEmpty() || value.length() >= length);
+        addCheck("minLength", value -> value.length() >= length);
         return this;
     }
 
     public StringSchema contains(String substring) {
-        addCheck("contains", value -> value.isEmpty() || value.contains(substring));
+        addCheck("contains", value -> value.contains(substring));
         return this;
     }
 }
